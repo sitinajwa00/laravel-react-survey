c00d58f689cd013b8980ae1fa1a2246028f2985f
sitinajwa00 <s.najwapandi@gmail.com>
2024-09-11 18:35:13 +0800

    Page component layout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,5 +1,6 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 import App from './App'
+import DefaultLayout from './components/DefaultLayout';
 import GuestLayout from './components/GuestLayout';
 import { Dashboard } from './views/Dashboard';
 import { Login } from './views/Login';
@@ -9,11 +10,21 @@ import { Surveys } from './views/Surveys';
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
-    },
-    {
-        path: '/surveys',
-        element: <Surveys />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/" />
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/surveys',
+                element: <Surveys />
+            }
+        ]
     },
     {
         path: '/',

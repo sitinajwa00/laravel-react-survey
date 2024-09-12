a3a424c9f65991f9deb762b115e1dbfab09311fc
sitinajwa00 <s.najwapandi@gmail.com>
2024-09-12 21:57:43 +0800

    ContextProvider

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,31 +1,33 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { NavLink, Outlet } from 'react-router-dom'
+import { UserStateContext } from '../views/contexts/ContextProvider'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+// const user = {
+//   name: 'Tom Cook',
+//   email: 'tom@example.com',
+//   imageUrl:
+//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// }
 const navigation = [
   { name: 'Dashboard', to: '/'},
   { name: 'Surveys', to: '/surveys'},
 ]
-const userNavigation = [
-  { name: 'Sign Out', href: '#' },
-]
+// const userNavigation = [
+//   { name: 'Sign Out', href: '#' },
+// ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function DefaultLayout() {
+  const { currentUser, userToken } = UserStateContext();
 
-    const logout = (ev) => {
-        ev.preventDefault();
-        alert('Logout');
-    }
+  const logout = (ev) => {
+      ev.preventDefault();
+      alert('Logout');
+  }
   return (
     <>
       <div className="min-h-full">
@@ -41,6 +43,7 @@ export default function DefaultLayout() {
                   />
                 </div>
                 <div className="hidden md:block">
+                {userToken}
                   <div className="ml-10 flex items-baseline space-x-4">
                     {navigation.map((item) => (
                       <NavLink
@@ -66,7 +69,7 @@ export default function DefaultLayout() {
                       <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="absolute -inset-1.5" />
                         <span className="sr-only">Open user menu</span>
-                        <img alt="" src={user.imageUrl} className="h-8 w-8 rounded-full" />
+                        <UserIcon className='w-8 h-8 bg-white/25 rounded-full p-1 text-white'/>
                       </MenuButton>
                     </div>
                     <MenuItems
@@ -117,11 +120,11 @@ export default function DefaultLayout() {
             <div className="border-t border-gray-700 pb-3 pt-4">
               <div className="flex items-center px-5">
                 <div className="flex-shrink-0">
-                  <img alt="" src={user.imageUrl} className="h-10 w-10 rounded-full" />
+                  <UserIcon className='w-8 h-8 bg-white/25 rounded-full p-1 text-white'/>
                 </div>
                 <div className="ml-3">
-                  <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                  <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                  <div className="text-base font-medium leading-none text-white">{currentUser.name}</div>
+                  <div className="text-sm font-medium leading-none text-gray-400">{currentUser.email}</div>
                 </div>
               </div>
               <div className="mt-3 space-y-1 px-2">
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import router from './router.jsx'
+import { ContextProvider } from './views/contexts/ContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </StrictMode>,
 )

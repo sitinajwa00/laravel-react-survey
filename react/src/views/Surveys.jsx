f8314d6503878afbb682c40a37c97d75f3ef6040
sitinajwa00 <s.najwapandi@gmail.com>
2024-09-14 01:16:08 +0800

    update SurveyListItem

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -6,12 +6,16 @@ import { useStateContext } from './contexts/ContextProvider'
 export const Surveys = () => {
   const { surveys } = useStateContext();
   console.log(surveys);
+
+  const onDeleteClick = () => {
+    console.log("On Delete click");
+  }
   return (
     <>
       <PageComponent title='Surveys'>
         <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3'>
           {surveys.map(survey => (
-            <SurveyListItem survey={survey} key={survey.id} />
+            <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick}/>
           ))}
         </div>
       </PageComponent>
